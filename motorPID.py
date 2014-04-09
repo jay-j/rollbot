33249a7d241d80d6c1c7b2a589142e77f4e69ba5
jay-j <jay-j@users.noreply.github.com>
2014-04-09 16:41:22 -0400

    Various updates; this powered the sys demo.

--- motorPID.py
+++ motorPID.py
@@ -3,70 +3,88 @@
 
 import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
-
+import math
 ######################### MOTOR FUNCTIONS #########################
 class motor:
-	def __init__(self, pin_pwm, pin_dir, pin_en, encoder_ref):
+	def __init__(self, pin_pwm, pin_for, pin_back, encoder_ref):
 		self.pin_pwm = pin_pwm
-		self.pin_dir = pin_dir
-		self.pin_en = pin_en
+		self.pin_for = pin_for
+		self.pin_back = pin_back
 
 		# startup pwm, stop
-		GPIO.setup(self.pin_dir, GPIO.OUT)
-		GPIO.setup(self.pin_en, GPIO.OUT)
-		self.stop()
+		GPIO.setup(self.pin_for, GPIO.OUT)
+		GPIO.setup(self.pin_back, GPIO.OUT)
+		#self.stop()
 		PWM.start(self.pin_pwm, 0.0)
 
 		# motor/pid params
 		self.kv = 0.1 # TODO
-		self.kp = 0.1 # TODO
+		self.kp = 10.0 # TODO
 		self.ki = 0.0 # TODO
 		self.kd = 0.0 # TODO
-		self.integralReset()
 
 		# save encoder class link
 		self.e = encoder_ref
-
-	def stop():
+		self.dist = 0;
+		self.ierror = 0
+	def stop(self):
+		PWM.set_duty_cycle(self.pin_pwm, 0)
 		# set enable pin low
-		GPIO.output(self.pin_en, GPIO.LOW)
+		#GPIO.output(self.pin_en, GPIO.LOW)
 
-	def setPWMFrequency(freq):
+	def setPWMFrequency(self,freq):
 		PWM.set_frequency(pin_pwm,freq)
 
-	def pid(xgoal, vgoal):
+	def resetPid(self):
+		self.dist = 0
+		self.integralReset()
+
+	def pid(self,xgoal, vgoal):
 		# feed-forwards term
 		ff = self.kv * vgoal
-
+		self.dist += self.e.get_delta() * .0203
+			
 		# pid calcs
-		error = self.e.x - xgoal
-		derror = -sign(error)*e.v
+		error = xgoal-self.dist
+		derror = 0#-sign(error)*e.v
 		self.ierror += error
 
 		pid_total = self.kp*error + self.ki*self.ierror + self.kd*derror + ff
-	
+
+		if pid_total > 9:
+			pid_total = 9
+		elif pid_total < -9:
+			pid_total = -9
+		
 		# stop motors if close to goal
-		if fabs(error) > 5:
+		if math.fabs(error) > 0.25:
 			self.run(pid_total)
+			return 0
 		else:
 			self.run(0)
+			return 1
+		
 
-	def run(volts):
+	def run(self,volts):
 		if volts != 0.0:
 			# volts is signed, -9 to 9
-			GPIO.output(self.pin_en, GPIO.HIGH)
+			#GPIO.output(self.pin_en, GPIO.HIGH)
 			if volts > 0:
-				GPIO.output(self.pin_dir, GPIO.HIGH)
+				GPIO.output(self.pin_back, GPIO.HIGH)
+				GPIO.output(self.pin_for, GPIO.LOW)
 			else:
-				GPIO.output(self.pin_dir, GPIO.LOW)
-		
+				GPIO.output(self.pin_back, GPIO.LOW)
+				GPIO.output(self.pin_for, GPIO.HIGH)
+
 			# rescale volts
-			volts = fabs(volts)*100./9.
+			volts = math.fabs(volts)*100./9.
+			if (volts > 100):
+				volts = 100
 			PWM.set_duty_cycle(self.pin_pwm, volts)
 		else:
 			self.stop()
 
-	def integralReset():
+	def integralReset(self):
 		self.ierror = 0
 
 
